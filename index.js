0ad4ac02a256a9bcba3e2f04239b1c3238a5a229
Jesse De Jong <jdoceau@gmail.com>
2021-07-31 13:14:16 +1000

    mockup inquirer question line with when function

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 // Import Dependencies
 const inquirer = require('inquirer');
 
-
+// Initilisation // Add a Manager
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {
+        name: 'name',
+        type: 'input',
+        message: 'What is the managers name?'
+    },
+    {
+        name: 'id',
+        type: 'input',
+        message: 'What is the managers employee id?'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'What is the managers email?'
+    },
+    {
+        name: 'officeNumber',
+        type: 'input',
+        message: 'What is the managers office number?'
+    },
+    {
+        name: 'continue',
+        type: 'confirm',
+        message: 'Would you like to add another employee?'
+    },
+    {
+        name: 'role',
+        type: 'list',
+        message: 'What is the employees role?',
+        choices: ['Engineer', 'Intern'],
+        when: function( answers ) {
+            return answers.continue === true;
+        }
+    }
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
   });
+
+// Create HTML call
