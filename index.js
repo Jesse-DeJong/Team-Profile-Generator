87a781ae143acab52980f85a985a5b8c873b0e0f
Jesse De Jong <jdoceau@gmail.com>
2021-07-31 12:54:07 +1000

    corrected classes to require base class, test processor.js created

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import Dependencies
+const inquirer = require('inquirer');
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  });
---
+++ src/processor.js
@@ -0,0 +1,9 @@
+// Import Dependencies
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+const Manager = require("../lib/Manager");
+
+const eg = new Manager("Jerry", 56, "jerry@gmail.com");
+
+console.log(eg);
